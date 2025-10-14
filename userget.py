628ba9cb56860527bc73f1ab985da58d30f208d6
Tharsan <tharsankartik2008@gmail.com>
2025-10-14 22:26:07 +0530

    Added a safer methodology by fetching from /etc/passwd rather than /etc/shadow for security reasons

--- userget.py
+++ userget.py
@@ -1,29 +1,45 @@
 #!/usr/bin/python
 
 
-# Path to the /etc/shadow file where the user information is stored
-# For format of the file check below given link
-# https://man.archlinux.org/man/shadow.5.en
-path1 = "/etc/shadow"
+def user_get():
+    # Path to the /etc/shadow file where the user information is stored
+    # For format of the file check below given link
+    # The /etc/passwd is more safe to use because encrypted pass is not stored there
+    # https://man.archlinux.org/man/shadow.5.en
+    # https://man.archlinux.org/man/passwd.5
+    path1 = "/etc/passwd"
 
-# Opening the File
-u = []
-with open(path1, 'r') as f:
+    # Opening the File
+    u = []  # --> user list
+    b = []  # --> background process list
+    with open(path1, 'r') as f:
 
-    # Reading the data is the File
-    data = f.readlines()
+        # Reading the data is the File
+        data = f.readlines()
 
-    # Each data string is ':' delimited therefore splitting with ':'
-    # If the second data(encrpted passwork) is "!*" then
-    # the user is a background process
-    for i in data:
-        d = i.split(":")
-        if d[1] == "!*":
-            pass
-        elif d[1] != "!*":
-            u.append(d[0])
+        # Each data string is ':' delimited therefore splitting with ':'
+        # If the third data(UID) is >= 1000, then it is a user.
+        # otherwise the user is a background process
+        for i in data:
+            d = i.split(":")
+            if int(d[2]) < 1000 or ('nologin' in d[6]):
+                b.append(d[0])
+            elif int(d[2]) >= 1000 or ('nologin' not in d[6]):
+                u.append(d[0])
+    return (u, b)
 
+
+# Utilizing the function
+u, b = user_get()
 # Printing the user stored
-print("USERNAMES IN THE SYSTEM")
-for i in u:
-    print(i)
+# print("USER IN THE SYSTEM")
+# print("++++++++++++++++++")
+# for i in u:
+# print(i)
+# print("++++++++++++++++++")
+# print()
+# print("BACKGROUND USERS")
+# print("++++++++++++++++++")
+# for i in b:
+# print(i)
+# print("++++++++++++++++++")

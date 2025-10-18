04170f83fcee0347972b52d9f4bf8ae1adfc834a
Tharsan <tharsankartik2008@gmail.com>
2025-10-18 23:43:09 +0530

    Some fun styling

--- user_add.py
+++ user_add.py
@@ -2,6 +2,8 @@
 
 
 import subprocess
+from rich.text import Text
+from rich.console import Console
 
 
 # The Main Function
@@ -20,3 +22,26 @@ def user_add(username, shell, groups, home=False):
 # and sys.argv and then construct a proper useradd command stored
 # in the cmd variable which is then executed using the subprocess
 # module
+
+
+# CLI USER INTEFACE CREATION
+
+
+# Console Creation
+con = Console()
+
+# Piping Output in subprocess
+cmd1 = subprocess.Popen(["figlet", "-c", "USER_ADD"], stdout=subprocess.PIPE)
+cmd2 = subprocess.Popen(
+    ["lolcat", "-a"], stdin=cmd1.stdout)
+cmd1.stdout.close()
+
+cmd2.communicate()
+
+
+# Introductory text
+intro = Text("WELCOME TO THE USER ADD CLI USER INTERFACE",
+             style="bold blue", justify="center")
+
+# Output Managment
+con.print(intro)
